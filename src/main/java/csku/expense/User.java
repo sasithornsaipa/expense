b11db9fbb0c06314a6cad2d9c06f4293ec11ecf9
sasithorn <sasithorn.sai@ku.th>
2018-08-31 14:18:43 +0700

    edit class

--- src/main/java/csku/expense/User.java
+++ src/main/java/csku/expense/User.java
@@ -1,7 +1,14 @@
 package csku.expense;
 
+import java.util.ArrayList;
+
 /**
  * Created by 708 on 8/31/2018.
  */
 public class User {
+    public String name;
+
+    public User(String name){
+        this.name = name;
+    }
 }
