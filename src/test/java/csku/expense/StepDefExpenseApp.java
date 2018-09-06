9a28f484095a4e8cb1476c6644d0849878d848f9
sasithorn <sasithorn.sai@ku.th>
2018-09-06 21:29:12 +0700

    finished

--- src/test/java/csku/expense/StepDefExpenseApp.java
+++ src/test/java/csku/expense/StepDefExpenseApp.java
@@ -11,21 +11,23 @@ public class StepDefExpenseApp {
     IncomeAccount account;
 
     @Before
-    void init(){
+    public void init(){
         account = new IncomeAccount();
     }
+
+
 
     @Given("a user have income with detail (.*) and balance (.*) exits")
     public void a_user_have_income_with_detail_and_balance_exits(String detail,double income){
         account.addTransaction("i", detail, income+"");
     }
 
-    @When("I add income with detail (.*) and balance (\\d+) exits")
+    @When("I add income with detail (.*) and balance (.*) exits")
     public void I_add_income_with_detail_and_balance_exits(String detail,String income){
         account.addTransaction("i", detail, income);
     }
 
-    @When("I add expense with detail (.*)  and balance (\\d+)  exits")
+    @When("I add expense with detail (.*) and balance (.*) exits")
     public void I_add_expense_with_detail_and_balance_exits(String detail,String income){
         account.addTransaction("e", detail, income);
     }
